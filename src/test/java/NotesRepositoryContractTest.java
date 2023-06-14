138d4e8a46b075be2ec6ca2a2ad39e529ba0ccc3
Johan Martinsson <jma@notsodark.com>
2023-06-14 17:50:47 +0200

    removed contract test

--- src/test/java/NotesRepositoryContractTest.java
+++ src/test/java/NotesRepositoryContractTest.java
@@ -1,12 +1,38 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import org.assertj.core.api.Assertions;
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.UUID;
 
-public abstract class NotesRepositoryContractTest {
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+public class NotesRepositoryContractTest {
     NotesRepository repository;
 
+    MongoClient mongoClient;
+    @BeforeEach
+    void setUp() {
+        CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+        mongoClient = MongoClients.create("mongodb://localhost");
+        MongoDatabase database = mongoClient.getDatabase("sample_mflix").withCodecRegistry(pojoCodecRegistry);
+
+
+        database.getCollection("movies", NoteEntity.class).drop();
+
+        repository = new MongoDbNotesRepository(database);
+
+    }
     @Test
     void can_create() {
         String noteId = "noteId";
@@ -45,4 +71,10 @@ public abstract class NotesRepositoryContractTest {
         repository.saveEntity(entity);
 
     }
+
+    @AfterEach
+    void tearDown() {
+        mongoClient.close();
+    }
+
 }
